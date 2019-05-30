18036fa5bdaedc830c9d95bcffe762b55eee0d17
19456w <19456w@gmail.com>
2019-05-30 20:00:28 +0200

    Add state and .map +bind

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
+import ReactHtmlParser from "react-html-parser";
 
 
 class Column extends React.Component {
+
     render() {
         return (
             <section className={styles.component}>
-                <h3 className={styles.title}>(this.props.titleText)</h3>
+                <h3 className={styles.title}>{ReactHtmlParser(this.props.titleText)}</h3>
             </section>
         )
     }
 }
+
 Column.propTypes = {
     titleText: PropTypes.node.isRequired,
 };
--- src/components/List/List.js
+++ src/components/List/List.js
@@ -47,7 +47,7 @@ class List extends React.Component {
                     {this.state.columns.map(({key, ...columnProps}) => (<Column key={key} {...columnProps} />
                     ))} </div>
                 <div className={styles.creator}>
-                    <Creator text={settings.columnCreatorText} action={title => this.addColumn(title)}/>
+                    <Creator text={settings.columnCreatorText} action={this.addColumn.bind(this)}/>
                 </div>
             </section>
 
